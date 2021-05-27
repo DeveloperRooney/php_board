09f7344a65c2ea665664a916a20df71d41cd0ebe
DeveloperRooney <han_0427@naver.com>
2021-05-28 01:57:23 +0900

    헤더

--- front/topmenu.php
+++ front/topmenu.php
@@ -1,26 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- 합쳐지고 최소화된 최신 CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/reset.css">
 
-    <!-- 부가적인 테마 -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
-    <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <!-- google font -->
     
-    <title>Document</title>
+
+    <title>topmenu</title>
 </head>
+
+<style>
+
+.topmenu-wrap {
+    background-color : #f2f3f7;
+}
+
+.topmenu-wrap ul{
+    height : 32px;
+    width : auto;
+    margin : 0 2%;
+}
+
+.topmenu-wrap ul li {
+    float: right;
+    margin-right : 14px;
+    line-height : 33px;
+    font-family: 'Noto Sans KR', sans-serif;
+}
+
+.topmenu-wrap::after {
+    content : "";
+    display: block;
+    clear : both;
+}
+
+.topmenu-top, .topmenu-mid {
+    width : 75%;
+    margin : 0 auto;
+    font-size : 12px;
+    color : #6c738a;
+}
+
+.topmenu-mid {
+    background-color : black;
+    height : 100px;
+}
+
+</style>
+
 <body>
-
-    <nav class="navbar navbar-inverse">
-    ...
+    <nav class="topmenu-wrap">
+        <div class="topmenu-top">
+            <ul>
+                <li>회원가입</li>
+                <li>로그인</li>
+            </ul>
+        </div>
     </nav>
-
 </body>
 </html>
