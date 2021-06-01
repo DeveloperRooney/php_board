15d89a1089f23d977e7a4df24806dac14dbdacad
DeveloperRooney <han_0427@naver.com>
2021-06-01 19:44:23 +0900

    MVC 구조화 연습

---
+++ mvc/app.php
@@ -0,0 +1,51 @@
+<?php
+
+
+class APP__ArticleRepository {
+
+
+    public function getForPrintArticles() : array {
+
+        $sql = "select * from article order by desc";
+
+        return DB__getRows($sql);
+    }
+
+}
+
+
+
+class APP__ArticleService {
+
+    private APP__ArticleRepository $articleRepository;
+
+    public function __construct()
+    {
+        $this->articleRepository = new APP__ArticleRepository();
+    }
+
+    public function getForPrintArticles() : array {
+        return $this->articleRepository->getForPrintArticles();
+    }
+
+}
+
+class APP_UserArticleController {
+
+    private APP__ArticleService $articleService;
+
+    public function __construct() {
+        $this->articleService = new APP_ArticleService();
+        
+    }
+
+    public function actionShowList() : array {
+        $articles = $this->articleService->getForPrintArticles();
+
+        return $articles;
+    }
+
+}
+
+
+?>
---
+++ mvc/util.php
@@ -0,0 +1,93 @@
+<?php
+
+class DB__SeqSql {
+    private string $templateStr;
+
+    private array $params;
+
+    public function add(string $sqlBit, string $param = null) {
+        $this->templateStr .= " " .$sqlBit;
+
+        if ($param) {
+            $this->params[] = $param;
+        }
+    }
+
+    public function getTemplate() : string {
+        return $this->templateStr;
+    }
+
+    public function getForBindParam1stArg() : string {
+        $paramTypesStr = "";
+
+        $count = count($this->params);
+
+        for ($i = 0; $i < $count; $i++) {
+            $paramTypesStr .= "s";
+        }
+        
+        return $paramTypesStr;
+
+    }
+
+
+    public function getParams() : array {
+        return $this->params;
+    }
+}
+
+
+// DB 연결을 위한 함수
+function DB__secSql() {
+
+
+    return new DB__SeqSql();
+}
+
+function DB__getRow2(DB__SeqSql $sql) {
+    global $dbConn;
+    $stmt = $dbConn->prepare($sql->getTemplate());
+    $stmt->bind_param($sql->getForBindParam1stArg(), ...$sql->getParams());
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    return $result->fetch_assoc();
+}
+
+
+
+
+function DB__getRow($sql) {
+
+    $dbConn = mysqli_connect("wayne94.cafe24.com", "wayne94", "suhan1994", "wayne94");
+
+    $result = mysqli_query($dbConn, $sql);
+
+    $article = mysqli_fetch_assoc($result);
+
+    return $article;
+}
+
+
+function DB__getRows($sql) : array{
+
+    $dbConn = mysqli_connect("wayne94.cafe24.com", "wayne94", "suhan1994", "wayne94");
+
+    $result = mysqli_query($dbConn, $sql);
+
+    $articles = [];
+
+    while ($result) {
+        $data = mysqli_fetch_assoc($result);
+
+        if ($data == null) {
+            break;
+        }
+
+        $articles[] = $data;
+    }
+
+    return $articles;
+}
+
+?>
